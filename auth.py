6a60fdbe674c183239dd9d3ac1bee130f97b9bc7
Victor Aquiles <victoraqalencar@gmail.com>
2020-03-20 20:04:34 -0300

    Start auth structure

--- auth.py
+++ auth.py
@@ -1,11 +1,59 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, request, session
+from flask_login import login_user, logout_user, login_required
+from flask_jwt_extended import create_access_token, create_refresh_token
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['POST'])
 def login():
-    return NotImplemented
 
-@auth.route('/signup')
+    data = request.get_json()
+
+    email = data['email']
+    password = data['password']
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user or not check_password_hash(user.password, password):
+        return {'message': 'Wrong credentials'}
+
+    login_user(user)
+
+    access_token = create_access_token(identity=email)
+    refresh_token = create_refresh_token(identity=email)
+
+    return {
+        'email': email,
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    }
+
+
+@auth.route('/signup', methods=['POST'])
 def signup():
-    return NotImplemented
+
+    data = request.get_json()
+
+    email = data['email']
+    password = data['password']
+    username = data['username']
+
+    user = User.query.filter_by(email=email).first()
+
+    if user: # if user already exists return a error message
+        return {'message': 'User already exists'}
+
+    new_user = User(email=email, username=username, password=generate_password_hash(password))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    access_token = create_access_token(identity=data['username'])
+    refresh_token = create_refresh_token(identity=data['username'])
+    return {
+        'email': email,
+        'access_token': access_token,
+        'refresh_token': refresh_token
+    }
+
--- routes.py
+++ routes.py
@@ -7,14 +7,14 @@ def index():
     return "Welcome to the Delivery Help"
 
 # Return the help board list
-@routes.route('/helpboard')
+@routes.route('/helpboard', methods=['GET'])
 def helpboard():
     return []
 
-@routes.route('/accept_demand')
+@routes.route('/accept_demand', methods=['POST'])
 def accept_demand():
     return NotImplemented
 
-@routes.route('/accept_volunteer')
+@routes.route('/accept_volunteer', methods=['POST'])
 def accept_volunteer():
     return NotImplemented
