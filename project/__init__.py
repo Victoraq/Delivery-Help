f2c223306935d5ce93f76c162cee30aecdff3009
gabriele <gabic.iwashima@gmail.com>
2020-03-20 17:21:13 -0300

    project folder

--- project/__init__.py
+++ project/__init__.py
@@ -1,21 +1,11 @@
 from flask import Flask
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
-from flask_bcrypt import Bcrypt
 from routes import routes
 from auth import auth
 
 app = Flask(__name__)
 
-app.config.from_pyfile('app.cfg')
-
-# Configure the database
+app.config['delivery-help'] = ' mysql873.umbler.com'
 db = SQLAlchemy(app)
-bcrypt = Bcrypt(app)
 
-# Routes blueprints
 app.register_blueprint(routes)
 app.register_blueprint(auth)
-
-login_manager = LoginManager()
-login_manager.init_app(app)
